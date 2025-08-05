8f1550ee4d961a28019900a2ab41dad6e7316e91
xxhusa <andre.rocha74@etec.sp.gov.br>
2025-08-04 22:26:35 -0300

    Add files via upload

---
+++ teste.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+echo "Boa noite";
+?>
+    <h1>boa noite </h1>
+
+    <?php echo "Falando sobre APACHE"; ?>
+
+    <a href = "pagina.php">TESTE</a>
+
+</body>
+</html>
+
